8f8d38a5d4cb974b62b945930e7f632abe871079
Dexter <dexedge@gmail.com>
2018-10-13 22:48:35 -0400

    First working version

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,17 @@
 $(document).ready( function (){
-  $('#static-table').DataTable();
-})
+  $('#ajax-table').DataTable({
+    ajax: '/get_dataset',
+    columns: [
+      {title: 'Date', data: 'date'},
+      {title: 'DOW', data: 'dow'},
+      {title: 'Month', data: 'month'},
+      {title: 'Title', data: 'title', className: 'title'},
+      {title: 'Genre', data: 'genre'},
+      {title: 'Receipts', data: 'receipts'},
+      {title: 'Zinz', data: 'zinz'},
+      {title: 'Double', data: 'double'},
+      {title: 'Composer', data: 'composer'},
+      {title: 'Author', data: 'author'}
+    ]
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :events do
     collection { post :import }
   end
+  root to: 'events#index'
+  get 'get_dataset', to: 'events#get_dataset'
 end
