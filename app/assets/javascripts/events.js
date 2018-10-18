165706e62c9f4a3fd2e436dbee4111a0c2b1d235
Dexter <dexedge@gmail.com>
2018-10-17 22:48:35 -0400

    Add stateSave; change background and font colors

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,13 +1,16 @@
 //  The DataTables plug-in absoluteOrder (in vendor/assets/javascripts) allows NULL values in Composer and Author always to be sorted to the bottom of the table, regardless of the direction of the sort. See: https://datatables.net/blog/2016-12-22
 
-// .on('turbolinks:load', function (){ is based on: https://stackoverflow.com/questions/30876402/datatables-only-loads-on-page-refresh. 
+// The .on('turbolinks:load', function (){ is based on: https://stackoverflow.com/questions/30876402/datatables-only-loads-on-page-refresh. 
 
-// It fixes the problem that datatable does not load without a refresh if coming via the "Back" link on a "Show" page. The flash and rewrite problem using this fix alone is solved acceptably by the code added in application.html.erb and index.html.erb
+// It fixes the problem of datatable not loading without a refresh if returning via the "Back" link on a "Show" page. This fix alone still has a flash and rewrite problem, which is solved acceptably by the code added at the top of application.html.erb and index.html.erb
+
+// stateSave: true retains the current state of the table upon returning to the page (for example, if there is a search on 'mozart')
 
 var noName = $.fn.dataTable.absoluteOrder({value: null, position: 'bottom'})
 
 $(document).on('turbolinks:load', function (){
   $('#ajax-table').DataTable({
+    stateSave: true,
     pageLength: 14,
     lengthMenu: [7, 14, 28, 56],
     ajax: '/get_dataset',
